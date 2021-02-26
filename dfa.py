65ee39e898174d288ba6bbd4795d930b67029a5a
Yixi Rao <u6826541@anu.edu.au>
2021-02-26 18:24:30 +0800

    Q3

--- dfa.py
+++ dfa.py
@@ -1,15 +1,15 @@
 """ File name:   dfa.py
-    Author:      <your name goes here>
-    Date:        <the date goes here>
+    Author:      Yixi Rao
+    Date:        25/02/2021
     Description: This file defines a function which reads in
-                 a DFA described in a file and builds an appropriate datastructure.
+                a DFA described in a file and builds an appropriate datastructure.
 
-                 There is also another function which takes this DFA and a word
-                 and returns if the word is accepted by the DFA.
+                There is also another function which takes this DFA and a word
+                and returns if the word is accepted by the DFA.
 
-                 It should be implemented for Exercise 3 of Assignment 0.
+                It should be implemented for Exercise 3 of Assignment 0.
 
-                 See the assignment notes for a description of its contents.
+                See the assignment notes for a description of its contents.
 """
 
 
@@ -26,8 +26,22 @@ def load_dfa(path_to_dfa_file):
 
         (str) -> Object
     """
-
-    # YOUR CODE HERE
+    initial_states = []
+    accepting_states = []
+    transitions = {}
+    with open(path_to_dfa_file) as dfa_file:
+        for line in dfa_file:
+            line = line[:-1]
+            cutting_line = line.split(" ")
+            name = cutting_line[0]
+            if name == "initial":
+                initial_states = cutting_line[1:]
+            elif name == "accepting":
+                accepting_states = cutting_line[1:]
+            else:
+                transitions[cutting_line[1] + " " + cutting_line[2]] = cutting_line[3]
+    return (initial_states, accepting_states, transitions)
+            
 
 
 def accepts_word(dfa, word):
@@ -38,4 +52,28 @@ def accepts_word(dfa, word):
         (Object, str) -> bool
     """
 
-    # YOUR CODE HERE
+    initial_states = dfa[0][0]
+    accepting_states = dfa[1]
+    transitions = dfa[2]
+
+    current_state = initial_states
+
+    for index in range(len(word) + 1):
+        if index == len(word):
+            if current_state in accepting_states:
+                return True
+            else:
+                return False
+        else:
+            current_char = word[index]
+            have_transition = False
+            for transition,label in transitions.items():
+                two_states = transition.split(" ")
+                if two_states[0] == current_state and label == current_char:
+                    current_state = two_states[1]
+                    have_transition = True
+                    break
+            if not have_transition:
+                return False       
+
+
